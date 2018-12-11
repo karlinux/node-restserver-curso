68d9a9ae8389a74fa966489bfbcad17454fe045d
ADMINISTRADOR <carcl69@hotmail.com>
2018-12-11 13:10:54 -0600

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://karlinux:Gua19200176@ds131814.mlab.com:31814/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
